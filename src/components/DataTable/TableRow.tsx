ac418d4ecfd9bb9e11b8a274ec975969afb59aea
mrsvolodya <vovamursky@gmail.com>
2025-02-08 15:58:18 +0200

    improve perfomance

--- src/components/DataTable/TableRow.tsx
+++ src/components/DataTable/TableRow.tsx
@@ -1,7 +1,7 @@
 import { TableCell } from "./TableCell";
 import { CellType } from "../../types/CellType";
 import { IconButton } from "../UI elements/IconButton/IconButton";
-import { memo, useCallback, useContext, useMemo } from "react";
+import { memo, useContext, useMemo } from "react";
 import { TableContext } from "../../contexts/TableContext";
 import { getRowTitle } from "../../utils/getRowTitle";
 import DeleteIcon from "../../assets/icon-del.svg";
@@ -18,24 +18,23 @@ const TableRowBase = ({ cells, rowID }: TableRowProps) => {
   const { deleteRow } = useContext(TableContext);
   const { isPersent } = useContext(HighlightContext);
 
-  const handleDelete = useCallback(() => deleteRow(rowID), [deleteRow, rowID]);
+  const handleDelete = () => deleteRow(rowID);
 
   const sum = cells.reduce((acc, col) => acc + col.amount, 0);
 
-  const percentages = useMemo(
-    () =>
-      isPersent.isActive && isPersent.id === rowID
-        ? getPersentOfSum(sum, cells)
-        : [],
-    [isPersent, rowID, sum, cells]
-  );
+  const percentages =
+    isPersent.isActive && isPersent.id === rowID
+      ? getPersentOfSum(sum, cells)
+      : [];
 
   const title = getRowTitle(cells[0].id);
-  const isSumHovered = isPersent.isActive && isPersent.id === rowID || '';
+  const isSumHovered = (isPersent.isActive && isPersent.id === rowID) || "";
 
   return (
     <>
-      <tr className={(style.table__row, isSumHovered && style[`table__row--sum`])}>
+      <tr
+        className={(style.table__row, isSumHovered && style[`table__row--sum`])}
+      >
         <th scope="row" className={style.table__cell}>
           <IconButton icon={DeleteIcon} onClick={handleDelete} title={title} />
         </th>
